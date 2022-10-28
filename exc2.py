224073a758f5976d72746f3582af0fb9b35d1789
Talita Hanna Cabral <talitalinden@gmail.com>
2022-10-28 15:32:40 -0300

    exc2.py

--- exc2.py
+++ exc2.py
@@ -6,14 +6,16 @@
 # não aceite a senha igual ao nome do usuário, 
 # mostrando uma mensagem de erro e voltando a pedir as informações. 
 
+#Faça um programa que leia um nome de usuário e a sua senha e 
+# não aceite a senha igual ao nome do usuário, 
+# mostrando uma mensagem de erro e voltando a pedir as informações. 
+
 nome = str(input("Digite o seu nome: "))
 senha = input("Digite a sua senha: ")
 
-while nome != senha:
-    print("A senha está correta.")
-    if nome == senha:
-        print("A senha deve ser diferente do seu nome.")
-    break
+while nome==senha:
+    print("A senha deve ser diferente do seu nome.")
+    senha = input("Digite a senha novamente: ")
 else:
-    print ("Digite a sua senha novamente: ")
+    print("Senha correta")
 
