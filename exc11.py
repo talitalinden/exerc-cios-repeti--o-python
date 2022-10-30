f51a51bcd240f1554791d363ca6cb7345c5e6e30
Talita Hanna Cabral <talitalinden@gmail.com>
2022-10-30 16:56:12 -0300

    exc11.py

---
+++ exc11.py
@@ -0,0 +1,7 @@
+#Altere o programa anterior para mostrar no final a soma dos números.
+
+num1 = int(input("Digite o primeiro número inteiro: "))
+num2 = int(input("Digite o segundo número inteiro: "))
+
+soma = num1 + num2
+print(f'A soma dos números é: {soma:.2f}')
